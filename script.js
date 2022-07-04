badfbe9b108e093d478a6310066145e184a2afee
DeBread <104747002+NotDeBread@users.noreply.github.com>
2022-07-03 23:36:44 -0500

    BETA v3.07
    -Added Breaxture pack page!! :D
    -Optimized some more code.
    -Small Fixes
    (The panorama in the Breaxture Pack page took way too long.)

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ var debread = document.getElementById('db');
 var debreadmad = false;
 var count = 3
 setInterval(dbcountdown, 1000)
+console.log("JS has been loaded!")
 
 debread.onclick = function animate() {
-    if (document.getElementById('db').src == "images/dbmad.png" && debreadmad == false) {
-        document.getElementById('db').src = "images/dbmad.png";
+    if (document.getElementById('db').src == "images/debread/dbmad.png" && debreadmad == false) {
+        document.getElementById('db').src = "images/debread/dbmad.png";
     } else {
-        document.getElementById('db').src = "images/dbmad.png";
+        document.getElementById('db').src = "images/debread/dbmad.png";
         debreadmad = true;
         count = 3
+        console.log("DeBread Has Been Clicked")
     }
 }
 
@@ -17,7 +19,8 @@ function dbcountdown() {
     count -= 1;
 
     if (count == 0 && debreadmad == true) {
-        document.getElementById('db').src = "images/db.png";
+        document.getElementById('db').src = "images/debread/db.png";
         debreadmad = true
+
     }
 }
