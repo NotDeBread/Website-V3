8b6c674322cc557223b84be2650812ff67c172a5
DeBread <104747002+NotDeBread@users.noreply.github.com>
2022-08-14 23:36:41 -0500

    BETA v3.11
    -Added Achievements. (There's only 1, ill add more soon.)
    -Removed warning text.
    -Added an addons section to the texture pack page.
    -Other minor improvements.

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 var debread = document.getElementById('db');
 var debreadmad = false;
 var count = 0
+var db_ach = 0
+var db_timesclicked = 0
+var db_ach_countdown = 0
 
 setInterval(dbcountdown, 1000)
+setInterval(db_achcountdown, 1000)
 console.log("Your maturnal figure is morbidly obese.")
 
 //Changes the .png to another .png and logs it.
@@ -12,9 +16,19 @@ debread.onclick = function animate() {
         document.getElementById('db').src = "images/debread/dbmad.png";
     } else {
         document.getElementById('db').src = "images/debread/dbmad.png";
+
+        db_timesclicked += 1;
         debreadmad = true;
-        count = 3
-        console.log('DeBread Has Been Clicked')
+        count = 3;
+        db_ach += 1;
+
+        if (db_ach == 1) {
+            document.getElementsByClassName("ach")[0].style.left = "10px";
+            console.log('Achievent Get!');
+            db_ach_countdown = 5;
+        }
+        document.getElementById("clickcounter").innerHTML = db_timesclicked;
+        console.log('DeBread Has Been Clicked (', db_timesclicked, ') Times.');
     }
 }
 
@@ -23,12 +37,20 @@ debread.onclick = function animate() {
 function dbcountdown() {
     if (count > 0) {
         count -= 1;
-        console.log('DeBread', count)
     } else return
 
     if (count == 0 && debreadmad == true) {
         document.getElementById('db').src = "images/debread/db.png";
         debreadmad = true
+    }
+}
 
+function db_achcountdown() {
+    if (db_ach_countdown > 0) {
+        db_ach_countdown -= 1;
+    } else return
+
+    if (db_ach_countdown == 0) {
+        document.getElementsByClassName("ach")[0].style.left = "-200px";
     }
 }
