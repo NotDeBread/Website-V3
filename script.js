88990759eee7ff7dfe4fbc5886eb2a9c0cdd764d
DeBread <104747002+NotDeBread@users.noreply.github.com>
2022-11-10 16:52:00 -0600

    v4.01
    -Fixed logo not displaying correctly
    -Eddie Accumulator BETA v0.16

--- script.js
+++ script.js
@@ -2,25 +2,9 @@ document.body.addEventListener('wheel', checkScrollDirection);
 
 function checkScrollDirection(event) {
   if (checkScrollDirectionIsUp(event)) {
-    document.getElementById('main').style.setProperty('height','100vh')
-
-    document.getElementById('arrow').style.setProperty('top','95vh')
-    document.getElementById('arrow').style.setProperty('opacity','1')
-
-    document.getElementById('arrowtext').style.setProperty('bottom','5vh')
-    document.getElementById('arrowtext').style.setProperty('opacity','1')
-
-    document.getElementById('bgtext').style.setProperty('opacity','1')
+    close_()
   } else {
-    document.getElementById('main').style.setProperty('height','75vh')
-
-    document.getElementById('arrow').style.setProperty('top','60vh')
-    document.getElementById('arrow').style.setProperty('opacity','0')
-
-    document.getElementById('arrowtext').style.setProperty('bottom','60vh')
-    document.getElementById('arrowtext').style.setProperty('opacity','0')
-
-    document.getElementById('bgtext').style.setProperty('opacity','0')
+    open_()
   }
 }
 
@@ -29,4 +13,28 @@ function checkScrollDirectionIsUp(event) {
     return event.wheelDelta > 0;
   }
   return event.deltaY < 0;
+}
+
+function open_() {
+  document.getElementById('main').style.setProperty('height','75vh')
+
+  document.getElementById('arrow').style.setProperty('top','60vh')
+  document.getElementById('arrow').style.setProperty('opacity','0')
+
+  document.getElementById('arrowtext').style.setProperty('bottom','60vh')
+  document.getElementById('arrowtext').style.setProperty('opacity','0')
+
+  document.getElementById('bgtext').style.setProperty('opacity','0')
+}
+
+function close_() {
+  document.getElementById('main').style.setProperty('height','100vh')
+
+  document.getElementById('arrow').style.setProperty('top','95vh')
+  document.getElementById('arrow').style.setProperty('opacity','1')
+
+  document.getElementById('arrowtext').style.setProperty('bottom','5vh')
+  document.getElementById('arrowtext').style.setProperty('opacity','1')
+
+  document.getElementById('bgtext').style.setProperty('opacity','1')
 }
